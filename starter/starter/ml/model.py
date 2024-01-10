6560259f41127fe70ffa890e11662e2a4cb15ad3
stfwi1104 <75065580+stfwi1104@users.noreply.github.com>
2024-01-10 15:50:03 +0100

    Update model.py
    Added Decision_tree

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn import tree
 
 
 # Optional: implement hyperparameter tuning.
@@ -17,8 +18,10 @@ def train_model(X_train, y_train):
     model
         Trained machine learning model.
     """
+    model = tree.DecisionTreeClassifier()
+    model = model.fit(X_train, y_train)
 
-    pass
+    return model
 
 
 def compute_model_metrics(y, preds):
