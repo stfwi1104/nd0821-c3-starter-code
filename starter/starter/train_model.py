a3595a7bf93a1709c8e2c064f38c8d2e0c003775
stfwi1104 <75065580+stfwi1104@users.noreply.github.com>
2024-01-10 15:48:25 +0100

    Update train_model.py
    X_train und X_test definiert

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,29 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+from ml.model import train_model 
+from ml.model import compute_model_metrics
+from sklearn import tree
+import pandas as pd
+import os
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+path = os.getcwd()
+parent_path = os.path.abspath(os.path.join(path, os.pardir))
+data_path = parent_path + "/data/"
+data_file = "census.csv"
+
+
+# Remove whitespaces
+data = pd.read_csv(data_path+data_file)
+for column in data.columns:
+    data = data.rename(columns={column : column.strip()})
+
+
+print(data.head())
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -24,5 +43,16 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+   test, categorical_features=cat_features, label="salary", training=False, encoder=encoder, lb=lb
+)
+
 
 # Train and save a model.
+model = train_model(X_train, y_train)
+
+# Calculate Model-Metrics
+preds = model.predict(X_test)
+precision, recall, fbeta = compute_model_metrics(y_test,preds)
+print(precision)
+
