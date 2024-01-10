9071415abaf425dc4de31745dc20bcb9a7978027
stfwi1104 <75065580+stfwi1104@users.noreply.github.com>
2024-01-10 22:05:29 +0100

    Update test_train_model.py

--- starter/starter/test_train_model.py
+++ starter/starter/test_train_model.py
@@ -5,17 +5,13 @@ import pytest
 import pickle
 import os
 
-'''
+
 @pytest.fixture
 def data():
     """ Simple function to generate some fake Pandas data."""
    
-    path = os.getcwd()
-    parent_path = os.path.abspath(os.path.join(path, os.pardir))
-    data_path = parent_path + "/data/"
-    data_file = "census.csv"
-    data = pd.read_csv(data_path+data_file)
-    print("fsaf")
+    data = pd.read_csv("data/census.csv")
+ 
     return data
 
 
@@ -29,7 +25,7 @@ def test_column_names(data):
     train, test = train_test_split(data, test_size=0.20)
     assert set(train.columns)==set(test.columns)
 
-'''
+
 def test_classifier():
     """ If your model is an instance of DeciontreeCLassifier then this is a valid test. """
     model = pickle.load(open("model/model.pkl",'rb'))
