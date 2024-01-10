96c6f3158920db6d9dc15b12a2af5be3c904b58d
stfwi1104 <75065580+stfwi1104@users.noreply.github.com>
2024-01-10 22:02:03 +0100

    Update test_train_model.py

--- starter/starter/test_train_model.py
+++ starter/starter/test_train_model.py
@@ -29,12 +29,12 @@ def test_column_names(data):
     train, test = train_test_split(data, test_size=0.20)
     assert set(train.columns)==set(test.columns)
 
-
+'''
 def test_classifier():
     """ If your model is an instance of DeciontreeCLassifier then this is a valid test. """
     model = pickle.load(open("/model/model.pkl",'rb'))
     assert isinstance(model, tree.DecisionTreeClassifier) 
-'''
+
 
 def test():
     """ If your model is an instance of DeciontreeCLassifier then this is a valid test. """
