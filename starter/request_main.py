232b859cadca5b6587b0ff20d1b1366df6ffb076
stfwi1104 <stfwi@web.de>
2024-01-12 21:09:02 +0100

    added testcase get/post

--- starter/request_main.py
+++ starter/request_main.py
@@ -18,13 +18,12 @@ featureinput =  { 'age':20,
             'native-country':"United-States"
             }
 
-'''
 
-response1 = requests.get('http://127.0.0.1:8000/')
-response2 = requests.post('http://127.0.0.1:8000/prediciton/', data=json.dumps(featureinput))
+
+response1 = requests.get('https://udacityproject-e5aeb4bbcf6e.herokuapp.com/')
+response2 = requests.post('https://udacityproject-e5aeb4bbcf6e.herokuapp.com/prediciton/', data=json.dumps(featureinput))
 
 print(response1.status_code)
 print(response1.json())
 print(response2.status_code)
 print(response2.json())
-'''
